67383cd1db94e958d250e156be200cc942b8db99
Tekkik <73826571+Tekkik@users.noreply.github.com>
2020-11-02 12:19:46 +0100

    Update BankUgyfelregi.js

--- BankUgyfelregi.js
+++ BankUgyfelregi.js
@@ -1,3 +1,3 @@
 function Registering(){
-	window.open("https://www.dropbox.com/sh/b2tch4m2400v75p/AABn5VtAIRlfg2Y5JpVU0T7Na?dl=0", "_self");
+	window.open("https://www.dropbox.com/sh/b2tch4m2400v75p/AABn5VtAIRlfg2Y5JpVU0T7Na?dl=0");
 }
